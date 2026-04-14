69c221045be7d5e4985fa80ee38489d929713a4a
Afsara Silvi Ami <afsarasilvi44@gmail.com>
2026-04-15 03:47:37 +0600

    made the error page

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router';
 const ErrorPage = () => {
     return (
         <div>
-            <div className='flex flex-col h-full justify-center items-center'>
-                <h1>404</h1>
-                <p>Page Can Not Be Found!</p>
-                <Link to="/"><button>Go Home</button></Link>
+            <div className='flex flex-col h-screen justify-center items-center space-y-5'>
+                <h1 className='text-8xl text-red-400 font-bold'>404</h1>
+                <p className='text-2xl text-gray-500'>Page Can Not Be Found!</p>
+                <Link to="/"><button className='btn btn-primary text-lg'>Go Home</button></Link>
             </div>
         </div>
     );
