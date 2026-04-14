d25a4deeaf0baa8e286e7bf7b0e1fa36af0d613a
Afsara Silvi Ami <afsarasilvi44@gmail.com>
2026-04-15 01:56:10 +0600

    did the Ui for timeline page

--- src/Components/Timeline/TimeLine.jsx
+++ src/Components/Timeline/TimeLine.jsx
@@ -3,6 +3,7 @@ import { TimelineContext } from './TimelineContext';
 import callImg from '../../assets/call.png'
 import textImg from "../../assets/text.png"
 import videoImg from '../../assets/video.png'
+import { FaAngleDown } from 'react-icons/fa';
 
 const TimeLine = () => {
 
@@ -11,18 +12,18 @@ const TimeLine = () => {
         if(activityItem.type === "call"){
             return (
                 <>
-                <span className='font-bold'>Call <span className='font-normal'>with {activityItem.friendName}</span></span>
+                <span className='font-medium text-[20px] text-[#244D3F]'>Call <span className='font-normal text-[18px] text-[#64748B]'>with {activityItem.friendName}</span></span>
                 </>
             )
         }
         else if(activityItem.type === "text"){
             return(<>
-                <span className='font-bold'>Text <span className='font-normal'>with {activityItem.friendName}</span></span>
+                <span className='font-medium text-[20px] text-[#244D3F]'>Text <span className='font-normal text-[18px] text-[#64748B]'>with {activityItem.friendName}</span></span>
                 </>)
         }
         else if(activityItem.type === "video"){
             return(<>
-                <span className='font-bold'>Video <span className='font-normal'>with {activityItem.friendName}</span></span>
+                <span className='font-medium text-[20px] text-[#244D3F]'>Video <span className='font-normal text-[18px] text-[#64748B]'>with {activityItem.friendName}</span></span>
                 </>)
         }
     }
@@ -42,29 +43,34 @@ const TimeLine = () => {
     return (
         <div>
             <div className='w-11/12 mx-auto'>
-                <h1 className='text-[48px] font-bold'>Timeline</h1>
+                <h1 className='text-[48px] font-bold mt-5'>Timeline</h1>
                 <div className="dropdown dropdown-center">
-  <div tabIndex={0} role="button" className="btn m-1">Click  ⬇️</div>
+  <div tabIndex={0} role="button" className="m-1 bg-base-300 py-2 px-6 text-[#64748B] rounded-md text-[18px] flex items-center gap-4">Filter timeline  <FaAngleDown></FaAngleDown></div>
   <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    <li><a>Item 1</a></li>
-    <li><a>Item 2</a></li>
+    <li><a>Call</a></li>
+    <li><a>Text</a></li>
+    <li><a>Video</a></li>
   </ul>
 </div>
-                {
+                <div>
+                    {
                     activity.length === 0?(<div><p>No activity yet</p></div>): (activity.map((item, index)=>{return(
-                        <div key={index}>
-                            <div className='flex'>
-                               <img src={getImage(item.type)} alt="" />
-                               <span> {getTimeline(item)}</span>
-                               
-                            </div>
-                            <div>
+                        <div key={index} className='shadow-sm p-4 my-3'>
+                            <div className='flex gap-2'>
+                               <img src={getImage(item.type)} alt="" className='h-10 ' />
+                               <span> {getTimeline(item)}
+                                     <div className='text-[16px] text-[#64748B]'>
                                 {item.time}
                             </div>
+                               </span>
+                               
+                            </div>
+                            
                           
                         </div>)
                     }))
                 }
+                </div>
             </div>
 
         </div>
